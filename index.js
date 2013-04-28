1da8ee4936a37ffa7017d335086ca1b1689ba902
Richard Hoffman <coverslide@gmail.com>
2013-04-28 01:33:21 +0000

    Let jzon be called by itself

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ module.exports = jzon
 jzon.parse = jzon('parse')
 jzon.stringify = jzon('stringify')
 
-function jzon(type){
-  return function(input, cb){
+function jzon(type, input, cb){
+  if(type != 'parse' && type != 'stringify')
+    throw new Error('Type argument must be parse or stringify')
+  if(typeof input != 'undefined')
+    return process(input, cb)
+  return process
+  function process(input, cb){
     try{
       var retval = JSON[type](input)
     } catch(e){
